47051ed024ed98f92cb1b9bbaefc723f1d1523a3
Doug Melton <doug@mobiata.com>
2015-08-03 00:55:20 -0700

    Simplified / customized requests for opensprinkler

--- opensprinkler.js
+++ opensprinkler.js
@@ -8,19 +8,24 @@ var port = process.argv[3];
 var password = process.argv[4];
 var hash = crypto.createHash('md5').update(password).digest('hex');
 
-var register = function(name, url, method) {
-    client.registerMethod(name, url, method);
-    return function(args) {
+var register = function(path) {
+    var url = "http://" + host + ":" + port + path;
+    return function(params) {
+        if (typeof params == 'undefined') {
+            params = {};
+        }
+        var args = { path: params };
+        args.path.pw = hash;
         return new Promise(function(resolve, reject) {
-            var req = client.methods[name](args, function(data, response) {
-                if (typeof data === undefined || typeof data.result === undefined) {
-                    reject("Unknown client response! " + JSON.stringify(data));
+            var req = client.get(url, args, function(data, response) {
+                if (typeof data == 'undefined' || typeof data.result == 'undefined') {
+                    reject('Unknown client response! ' + JSON.stringify(data));
                 }
-                else if (typeof data.result != undefined || data.result == '1') {
+                else if (typeof data.result == 'undefined' || data.result == '1') {
                     resolve(data);
                 }
                 else {
-                    reject("Invalid result: " + data.result);
+                    reject('Invalid result: ' + data.result);
                 }
             });
             req.on('error', function(err) {
@@ -31,31 +36,44 @@ var register = function(name, url, method) {
 };
 
 // Get station names
-var jn = register("jn", "http://" + host + ":" + port + "/jn?pw=${pw}", "GET");
+var jn = register("/jn?pw=${pw}");
 
 // Get station status
-var js = register("js", "http://" + host + ":" + port + "/js?pw=${pw}", "GET");
+var js = register("/js?pw=${pw}");
 
 // Manual station run
 // sid: Station index (starting from 0)
 // en: Enable bit (1: open the selected station; 0: close the selected station).
 // t: Timer (in seconds). Acceptable range is 0 to 64800 (18 hours). The timer value must be provided if opening a station.
-var cm = register("cm", "http://" + host + ":" + port + "/cm?pw=${pw}&sid=${sid}&en=${en}&t={t}", "GET");
+var cm = register("/cm?pw=${pw}&sid=${sid}&en=${en}&t={t}");
 
 // Start Run-Once Program
 // Starts a run-once program for the given duration for each station defined in t, as an array
 // t: An array of station durations, one for each station: [60,0,0,15,15,0]
-var cr = register("cr", "http://" + host + ":" + port + "/cr?pw=${pw}&t={t}", "GET");
+var cr = register("/cr?pw=${pw}&t={t}");
  
-jn({ path: { pw: hash } }).then(function(data) {
-    console.log("Station Names:\n==============\n", data.snames);
-    return js({ path: { pw: hash } });
+var stations = [];
+
+jn().then(function(data) {
+    for (var i = 0; i < data.snames.length; i++) {
+        if (stations[i] == undefined) {
+            stations[i] = {};
+        }
+        stations[i].name = data.snames[i];
+    }
+    return js();
 }).then(function(data) {
-    console.log("Station Status:\n===============\n", data.sn);
-//    return cm({ path: { pw: hash, sid: 3, en: 1, t: 30 } });
+    for (var i = 0; i < data.sn.length; i++) {
+        if (stations[i] == undefined) {
+            stations[i] = {};
+        }
+        stations[i].status = data.sn[i];
+    }
+    console.log("Stations:\n===============\n", stations);
+//    return cm({ sid: 3, en: 1, t: 30 });
 //}).then(function(data) {
 //    console.log("Manual station run:\n", data);
-//    return js({ path: { pw: hash } });
+//    return js();
 //}).then(function(data) {
 //    console.log("Station Status:\n===============\n", data.sn);
 }).catch(function(err) {
